b8c7dc7ea118b8fa4e493933fe37266d4454f85d
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-08-01 10:33:05 -0700

    remove link from header

--- movie-suggester/src/components/header.js
+++ movie-suggester/src/components/header.js
@@ -1,11 +1,7 @@
-import { Link } from "react-router-dom";
-
 function Header() {
   return (
     <header>
-      <Link to="/">
-        <h1>Movie Suggester</h1>
-      </Link>
+      <h1>Movie Suggester</h1>
     </header>
   );
 }
